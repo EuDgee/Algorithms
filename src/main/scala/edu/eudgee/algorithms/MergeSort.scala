526752853fbcbc2261d3ac0329763c7390ff639e
Evgenii Mikheev <eudgee@gmail.com>
2015-01-20 22:09:32 +0300

    merge sort

--- src/main/scala/edu/eudgee/algorithms/MergeSort.scala
+++ src/main/scala/edu/eudgee/algorithms/MergeSort.scala
@@ -1,5 +1,42 @@
 package edu.eudgee.algorithms
 
+import scala.collection.mutable.ArrayBuffer
+
 object MergeSort {
-  def sort(unsorted: Array[Int]): Array[Int] = unsorted
+  def sort(array: Array[Int]): Array[Int] = {
+    if (array.length == 1) {
+      array
+    } else {
+      val divided = divide(array)
+      merge(sort(divided._1), sort(divided._2))
+    }
+  }
+
+  def divide(array: Array[Int]): (Array[Int], Array[Int]) = {
+    val dividedBy = array.length / 2
+    (array take dividedBy, array takeRight array.length - dividedBy)
+  }
+
+  def merge(array1: Array[Int], array2: Array[Int]): Array[Int] = {
+    val result = ArrayBuffer[Int]()
+    var i = 0
+    var j = 0
+    while (i < array1.length && j < array2.length) {
+      if (array1(i) < array2(j)) {
+        result append array1(i)
+        i += 1
+      } else {
+        result append array2(j)
+        j += 1
+      }
+    }
+
+    if (i < array1.length) {
+      result appendAll array1.takeRight(array1.length - i)
+    } else {
+      result appendAll array2.takeRight(array2.length - j).toList
+    }
+
+    result.toArray
+  }
 }
--- src/test/scala/edu/eudgee/algorithms/MergeSortSpec.scala
+++ src/test/scala/edu/eudgee/algorithms/MergeSortSpec.scala
@@ -1,9 +1,28 @@
 package edu.eudgee.algorithms
 
+import org.scalacheck.Gen
 import org.scalatest._
+import org.scalacheck.Prop.forAll
+
 
 class MergeSortSpec extends FlatSpec with Matchers {
-  "A Merge Sort" should "sort an array" in {
+  "A Merge Sort" should "sort a simple array" in {
+    val unsortedArray: Array[Int] =
+      Array(0, 1, 2)
+    val sortedArray: Array[Int] =
+      Array(0, 1, 2)
+    MergeSort.sort(unsortedArray) should be (sortedArray)
+  }
+
+  "A Merge Sort" should "sort another simple array" in {
+    val unsortedArray: Array[Int] =
+      Array(2, 0, 1)
+    val sortedArray: Array[Int] =
+      Array(0, 1, 2)
+    MergeSort.sort(unsortedArray) should be (sortedArray)
+  }
+
+  "A Merge Sort" should "sort any array" in {
     val unsortedArray: Array[Int] =
         Array(2, 123, 45, 34, 235, 3, 53, 34, 3, 0, 34, 345, 1, 346, 54)
     val sortedArray: Array[Int] =
